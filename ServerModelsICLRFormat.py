39e3a47a70986184340ab43e53747c74e8781112
Caleb Robinson <calebrob6@gmail.com>
2019-02-15 11:56:35 -0800

    0. import cntk broke everything somehow

--- ServerModelsICLRFormat.py
+++ ServerModelsICLRFormat.py
@@ -71,4 +71,8 @@ class KerasModel(object):
             output[y:y+self.input_size, x:x+self.input_size] += model_output[i] * kernel[..., np.newaxis]
             counts[y:y+self.input_size, x:x+self.input_size] += kernel
 
-        return output / counts[..., np.newaxis]
+        output = output / counts[..., np.newaxis]
+        output[:,:,4] += output[:,:,5]
+        output[:,:,4] += output[:,:,6]
+        output = output[:,:,:5]
+        return output
