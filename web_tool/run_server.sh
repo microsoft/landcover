e52889b7790877f2d273636eac53cb486ef252c8
Anthony Ortiz Cepeda <t-anorti@microsoft.com>
2019-04-20 19:29:52 -0700

    fixing finetune

--- web_tool/run_server.sh
+++ web_tool/run_server.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 MODEL="nips_gn"
-MODEL_FN="/mnt/blobfuse/train-output/conditioning/models/backup_unet_gn_isotropic_nn8/training/checkpoint_best.pth.tar"
+MODEL_FN="/mnt/blobfuse/train-output/conditioning/models/backup_unet_gn_isotropic_nn9/training/checkpoint_best.pth.tar"
 #MODEL_FN="/mnt/blobfuse/train-output/conditioning/models/backup_fusionnet32_gn_8_isotropic/training/checkpoint_best.pth.tar"
 
 python -u backend_server.py \
