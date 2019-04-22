2aaea698edd9d9f1a3abac2843996c255b11a582
Anthony Ortiz Cepeda <t-anorti@microsoft.com>
2019-04-22 13:51:55 -0700

    Finetuning ready

--- web_tool/ServerModelsNIPSGroupNorm.py
+++ web_tool/ServerModelsNIPSGroupNorm.py
@@ -149,7 +149,7 @@ class UnetgnFineTune(BackendModel):
         return output
 
 #FIXME: add retrain method
-    def retrain(self, train_steps=20, corrections_from_ui=True, learning_rate=0.0045):
+    def retrain(self, train_steps=10, corrections_from_ui=True, learning_rate=0.006):
         num_labels = np.count_nonzero(self.correction_labels)
         print("Fine tuning group norm params with %d new labels. 4 Groups, 8 Params" % num_labels)
 
