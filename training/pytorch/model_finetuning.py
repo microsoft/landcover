be7612a063cf4bbee59b69c39fa6ae3a1d0dc285
Blake Elias <blakeelias@gmail.com>
2019-04-26 02:12:29 +0000

    Fix errors in model finetuning; for speed, don't calculate train IoU

--- training/pytorch/model_finetuning.py
+++ training/pytorch/model_finetuning.py
@@ -254,7 +254,7 @@ def train_model(model, criterion, optimizer, scheduler, dataloaders, hyper_param
                     val_mean_IoU = meanIoU / n_iter
                 elif phase == 'train':
                     train_loss = running_loss / n_iter
-                    train_mean_IoU = meanIoU / n_iter
+                    #train_mean_IoU = meanIoU / n_iter
 
             #print('{} Loss: {:.4f} Acc: {:.4f}'.format(
             #      phase, epoch_loss, epoch_mean_IoU))
@@ -262,7 +262,7 @@ def train_model(model, criterion, optimizer, scheduler, dataloaders, hyper_param
             'run_id': hyper_parameters['run_id'],
             'hyper_parameters': hyper_parameters,
             'epoch': epoch,
-            'train_IoU': train_mean_IoU,
+      #      'train_IoU': train_mean_IoU,
             'train_loss': train_loss,
             'val_IoU': val_mean_IoU,
             'val_loss': val_loss,
@@ -282,7 +282,7 @@ def train_model(model, criterion, optimizer, scheduler, dataloaders, hyper_param
     duration = datetime.now() - since
     seconds_elapsed = duration.total_seconds()
 
-    
+
     print('Training complete in {:.0f}m {:.0f}s'.format(
         seconds_elapsed // 60, seconds_elapsed % 60))
     print('Best val IoU: {:4f}'.format(best_mean_IoU))
@@ -369,13 +369,13 @@ if __name__ == "__main__":
 
         'optimizer_method': [torch.optim.Adam], #, torch.optim.SGD],
         'last_k_layers': [1,], #2, 4, 8],
-        'learning_rate': [0.05, 0.01, 0.005, 0.001],
+        'learning_rate': [0.03, 0.01, 0.005, 0.001],
         'lr_schedule_step_size': [7],
     }
 
     params_sweep_group_norm = {
         'optimizer_method': [torch.optim.Adam], #, torch.optim.SGD],
-        'learning_rate': [0.05, 0.01, 0.005, 0.001],
+        'learning_rate': [0.03, 0.01, 0.005, 0.001],
         'lr_schedule_step_size': [7],
     }
 
