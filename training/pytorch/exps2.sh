0272d52010f99bbb008f4722c3cef7c84d82a43b
Blake Elias <blakeelias@gmail.com>
2019-04-27 05:00:34 +0000

    Make 2 shell scripts to parallelize jobs

--- training/pytorch/exps2.sh
+++ training/pytorch/exps2.sh
@@ -1,6 +1,6 @@
-export CUDA_VISIBLE_DEVICES=0
+export CUDA_VISIBLE_DEVICES=1
 
-for test_region in {1..2}
+for test_region in {3..4}
 do
 
     # Train fine-tuned model
@@ -18,6 +18,9 @@ do
     
     # Test original model
     python training/pytorch/test_finetuning.py --model_file "/mnt/blobfuse/train-output/conditioning/models/backup_unet_gn_isotropic_nn9/training/checkpoint_best.pth.tar" --test_tile_fn training/data/finetuning/test${test_region}.txt
+
+    export CUDA_VISIBLE_DEVICES=0
+    export CUDA_VISIBLE_DEVICES=1
     
 
 done
