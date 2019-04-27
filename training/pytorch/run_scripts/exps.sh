956bb45ee9f23fd562843cdef6014d5a312c3207
Blake Elias <blakeelias@gmail.com>
2019-04-27 05:45:49 +0000

    Add command line arguments for train script

--- training/pytorch/run_scripts/exps.sh
+++ training/pytorch/run_scripts/exps.sh
@@ -1,23 +1,46 @@
-export CUDA_VISIBLE_DEVICES=0
+while getopts ":t:g:" opt; do
+    case ${opt} in
+	g )
+	    CUDA_VISIBLE_DEVICES=$OPTARG
+	    ;;
+	t )
+	    TEST_REGION=$OPTARG
+	    ;;
+	\? )
+	    echo "Invalid option: $OPTARG" 1>&2
+	    ;;
+	: )
+	    echo "Invalid option: $OPTARG requires an argument" 1>&2
+	    ;;
+    esac
+done
+shift $((OPTIND -1))
 
-for test_region in {1..2}
+echo "gpu"
+echo $CUDA_VISIBLE_DEVICES
+echo "test region"
+echo $TEST_REGION
+
+
+#conda activate py35
+#export PYTHONPATH=../../..
+export PYTHONPATH=.
+
+# Train fine-tuned model
+python training/pytorch/model_finetuning.py --model_file "/mnt/blobfuse/train-output/conditioning/models/backup_unet_gn_isotropic_nn9/training/checkpoint_best.pth.tar" --training_patches_fn "training/data/finetuning/test${TEST_REGION}_train_patches.txt" --log_fn "/mnt/blobfuse/train-output/conditioning/models/backup_unet_gn_isotropic_nn9/finetuning/test/test$TEST_REGION/finetune.csv" --model_output_directory "/mnt/blobfuse/train-output/conditioning/models/backup_unet_gn_isotropic_nn9/finetuning/test/test${TEST_REGION}"
+
+
+# Test fine-tuned models
+for mask_id in {0..11}
 do
-
-    # Train fine-tuned model
-    python training/pytorch/model_finetuning.py --model_file "/mnt/blobfuse/train-output/conditioning/models/backup_unet_gn_isotropic_nn9/training/checkpoint_best.pth.tar" --training_patches_fn "training/data/finetuning/test${test_region}_train_patches.txt" --log_fn "/mnt/blobfuse/train-output/conditioning/models/backup_unet_gn_isotropic_nn9/test/finetune.csv" --model_output_directory "/mnt/blobfuse/train-output/conditioning/models/backup_unet_gn_isotropic_nn9/finetuning/test/test${test_region}"
-
-
-    # Test fine-tuned models
-    for mask_id in {0..11}
-    do
-    python training/pytorch/test_finetuning.py --model_file "/mnt/blobfuse/train-output/conditioning/models/backup_unet_gn_isotropic_nn9/finetuning/test/test${test_region}/finetuned_unet_gn.pth_{'lr_schedule_step_size': 5, 'optimizer_method': <class 'torch.optim.adam.Adam'>, 'run_id': 1, 'method_name': 'last_k_layers', 'epoch': 9, 'last_k_layers': 1, 'learning_rate': 0.01, 'mask_id': $mask_id}.tar" --test_tile_fn training/data/finetuning/test${test_region}.txt
-    python training/pytorch/test_finetuning.py --model_file "/mnt/blobfuse/train-output/conditioning/models/backup_unet_gn_isotropic_nn9/finetuning/test/test${test_region}/finetuned_unet_gn.pth_{'lr_schedule_step_size': 5, 'optimizer_method': <class 'torch.optim.adam.Adam'>, 'run_id': 1, 'method_name': 'last_k_layers', 'epoch': 9, 'last_k_layers': 2, 'learning_rate': 0.01, 'mask_id': $mask_id}.tar" --test_tile_fn training/data/finetuning/test${test_region}.txt
-    python training/pytorch/test_finetuning.py --model_file "/mnt/blobfuse/train-output/conditioning/models/backup_unet_gn_isotropic_nn9/finetuning/test/test${test_region}/finetuned_unet_gn.pth_{'lr_schedule_step_size': 5, 'optimizer_method': <class 'torch.optim.adam.Adam'>, 'run_id': 1, 'method_name': 'last_k_layers', 'epoch': 9, 'last_k_layers': 4, 'learning_rate': 0.01, 'mask_id': $mask_id}.tar" --test_tile_fn training/data/finetuning/test${test_region}.txt
-    python training/pytorch/test_finetuning.py --model_file "/mnt/blobfuse/train-output/conditioning/models/backup_unet_gn_isotropic_nn9/finetuning/test/test${test_region}/finetuned_unet_gn.pth_{'lr_schedule_step_size': 5, 'optimizer_method': <class 'torch.optim.adam.Adam'>, 'run_id': 1, 'method_name': 'group_params', 'epoch': 9, 'learning_rate': 0.03, 'mask_id': $mask_id}.tar" --test_tile_fn training/data/finetuning/test${test_region}.txt
+    python training/pytorch/test_finetuning.py --model_file "/mnt/blobfuse/train-output/conditioning/models/backup_unet_gn_isotropic_nn9/finetuning/test/test${TEST_REGION}/finetuned_unet_gn.pth_{'lr_schedule_step_size': 5, 'optimizer_method': <class 'torch.optim.adam.Adam'>, 'run_id': 1, 'method_name': 'last_k_layers', 'epoch': 9, 'last_k_layers': 1, 'learning_rate': 0.01, 'mask_id': $mask_id}.tar" --test_tile_fn training/data/finetuning/test${TEST_REGION}.txt
+    python training/pytorch/test_finetuning.py --model_file "/mnt/blobfuse/train-output/conditioning/models/backup_unet_gn_isotropic_nn9/finetuning/test/test${TEST_REGION}/finetuned_unet_gn.pth_{'lr_schedule_step_size': 5, 'optimizer_method': <class 'torch.optim.adam.Adam'>, 'run_id': 1, 'method_name': 'last_k_layers', 'epoch': 9, 'last_k_layers': 2, 'learning_rate': 0.01, 'mask_id': $mask_id}.tar" --test_tile_fn training/data/finetuning/test${TEST_REGION}.txt
+    python training/pytorch/test_finetuning.py --model_file "/mnt/blobfuse/train-output/conditioning/models/backup_unet_gn_isotropic_nn9/finetuning/test/test${TEST_REGION}/finetuned_unet_gn.pth_{'lr_schedule_step_size': 5, 'optimizer_method': <class 'torch.optim.adam.Adam'>, 'run_id': 1, 'method_name': 'last_k_layers', 'epoch': 9, 'last_k_layers': 4, 'learning_rate': 0.01, 'mask_id': $mask_id}.tar" --test_tile_fn training/data/finetuning/test${TEST_REGION}.txt
+    python training/pytorch/test_finetuning.py --model_file "/mnt/blobfuse/train-output/conditioning/models/backup_unet_gn_isotropic_nn9/finetuning/test/test${TEST_REGION}/finetuned_unet_gn.pth_{'lr_schedule_step_size': 5, 'optimizer_method': <class 'torch.optim.adam.Adam'>, 'run_id': 1, 'method_name': 'group_params', 'epoch': 9, 'learning_rate': 0.03, 'mask_id': $mask_id}.tar" --test_tile_fn training/data/finetuning/test${TEST_REGION}.txt
     done
     
     # Test original model
-    python training/pytorch/test_finetuning.py --model_file "/mnt/blobfuse/train-output/conditioning/models/backup_unet_gn_isotropic_nn9/training/checkpoint_best.pth.tar" --test_tile_fn training/data/finetuning/test${test_region}.txt
+    python training/pytorch/test_finetuning.py --model_file "/mnt/blobfuse/train-output/conditioning/models/backup_unet_gn_isotropic_nn9/training/checkpoint_best.pth.tar" --test_tile_fn training/data/finetuning/test${TEST_REGION}.txt
     
 
 done
