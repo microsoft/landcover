deba78b8fb147d6323b5aa7cdc0c0feb501ca527
Ubuntu <t-anorti@anthony-dlvm-v3.3hp3421ppnpe1a3p2z5w2fgm0c.bx.internal.cloudapp.net>
2019-04-30 01:30:39 +0000

    Group norm ready for label finetuning

--- web_tool/ServerModelsNIPSGroupNorm.py
+++ web_tool/ServerModelsNIPSGroupNorm.py
@@ -113,7 +113,7 @@ class UnetgnFineTune(BackendModel):
         self.correction_labels = None
         self.tile_padding = 0
 
-        self.down_weight_padding = 10
+        self.down_weight_padding = 40
 
         self.stride_x = self.input_size - self.down_weight_padding * 2
         self.stride_y = self.input_size - self.down_weight_padding * 2
@@ -149,7 +149,7 @@ class UnetgnFineTune(BackendModel):
         return output
 
 #FIXME: add retrain method
-    def retrain(self, train_steps=7, corrections_from_ui=True, learning_rate=0.03):
+    def retrain(self, train_steps=6, corrections_from_ui=True, learning_rate=0.005):
         num_labels = np.count_nonzero(self.correction_labels)
         print("Fine tuning group norm params with %d new labels. 4 Groups, 8 Params" % num_labels)
 
