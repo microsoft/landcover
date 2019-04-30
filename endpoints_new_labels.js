5f3d0c64cb191eb163b361f7e6576dd331324961
Caleb Robinson <calebrob6@gmail.com>
2019-04-30 01:55:02 -0700

    0. Adding multiple backend functionality

--- endpoints_new_labels.js
+++ endpoints_new_labels.js
@@ -1,3 +1,11 @@
 ENDPOINTS = [
     {"name": "hr_md-dense", "url": "http://msrcalebubuntu.eastus.cloudapp.azure.com:4445/"}
 ];
+
+BACKEND_ENDPOINTS = [
+    {"url": "http://msrcalebubuntu.eastus.cloudapp.azure.com:4444/"},
+    {"url": "http://msrcalebubuntu2.eastus.cloudapp.azure.com:4444/"},
+    {"url": "http://msrcalebubuntu2.eastus.cloudapp.azure.com:4445/"},
+    {"url": "http://msrcalebubuntu2.eastus.cloudapp.azure.com:4446/"},
+    {"url": "http://msrcalebubuntu2.eastus.cloudapp.azure.com:4447/"}
+]
