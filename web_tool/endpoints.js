6ce016795f74b0ecd3c868d38b58426c1f5d6e57
anthonymlortiz <anthonymlortiz@gmail.com>
2019-05-02 12:48:50 -0700

    changing endpoints

--- web_tool/endpoints.js
+++ web_tool/endpoints.js
@@ -3,7 +3,7 @@ ENDPOINTS = [
 ];
 
 BACKEND_ENDPOINTS = [
-    {"url": "http://msrcalebubuntu.eastus.cloudapp.azure.com:4444/"},
+    {"url": "http://anthonygn.eastus.cloudapp.azure.com:4444/"},
     {"url": "http://msrcalebubuntu2.eastus.cloudapp.azure.com:4444/"},
     {"url": "http://msrcalebubuntu2.eastus.cloudapp.azure.com:4445/"},
     {"url": "http://msrcalebubuntu2.eastus.cloudapp.azure.com:4446/"},
