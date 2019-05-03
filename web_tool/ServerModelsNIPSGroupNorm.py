c9e0c42cce6bd360e26deb9027d5df556b066fee
anthonymlortiz <anthonymlortiz@gmail.com>
2019-05-02 19:55:50 -0700

    Remove extra padding

--- web_tool/ServerModelsNIPSGroupNorm.py
+++ web_tool/ServerModelsNIPSGroupNorm.py
@@ -223,8 +223,8 @@ class UnetgnFineTune(BackendModel):
         return success, message
 
     def add_sample(self, tdst_row, bdst_row, tdst_col, bdst_col, class_idx):
-        pdb.set_trace()
-        padding = 0
+       # pdb.set_trace()
+        padding = self.tile_padding
         print("adding sample: class %d (incremented to %d) at (%d, %d)" % (class_idx, class_idx + 1, tdst_row, tdst_row))
         self.correction_labels[tdst_row + padding: bdst_row + 1 + padding,
                                tdst_col + padding: bdst_col + 1 + padding, :] = 0.0
