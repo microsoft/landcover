10f8ed34f25e8cd851480787dd769797617c0f9c
anthonymlortiz <anthonymlortiz@gmail.com>
2019-05-02 20:38:43 -0700

    Remove extra padding

--- web_tool/ServerModelsNIPSGroupNorm.py
+++ web_tool/ServerModelsNIPSGroupNorm.py
@@ -150,7 +150,7 @@ class UnetgnFineTune(BackendModel):
 #FIXME: add retrain method
     def retrain(self, train_steps=200, corrections_from_ui=True, learning_rate=0.0015):
         print_every_k_steps = 1
-       # pdb.set_trace()
+        pdb.set_trace()
         print('In retrain')
         num_labels = np.count_nonzero(self.correction_labels)
         height = self.naip_data.shape[1]
