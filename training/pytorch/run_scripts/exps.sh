ee20e831ce06d86ca5e47c0896cacfcd2d51402a
Blake Elias <blelias@microsoft.com>
2019-05-10 07:13:34 +0000

    Un-comment for loops... actually sweeping over all configurations now.

--- training/pytorch/run_scripts/exps.sh
+++ training/pytorch/run_scripts/exps.sh
@@ -29,18 +29,18 @@ python training/pytorch/test_finetuning.py --area test${TEST_REGION} --model_fil
 python training/pytorch/test_finetuning.py --area test${TEST_REGION} --model_file "/mnt/blobfuse/train-output/conditioning/models/backup_unet_gn_isotropic_nn9/finetuning/baseline_models/baseline_unet_group_params_isotropic_nn9.pth.tar" --test_tile_fn training/data/finetuning/test${TEST_REGION}_test_tiles.txt
 
 
-#for num_patches in ${NUMS_PATCHES[*]}
-#do
+for num_patches in ${NUMS_PATCHES[*]}
+do
 num_patches=40
 
     MODELS_DIR="/mnt/blobfuse/train-output/conditioning/models/backup_unet_gn_isotropic_nn9/finetuning/test/test${TEST_REGION}/${num_patches}_patches"
 
     random_seed=1
-#    for random_seed in {1..5}
+    for random_seed in {1..5}
 		       
-#    do
+    do
 	# Train fine-tuned model
-	# python training/pytorch/model_finetuning.py --model_file "/mnt/blobfuse/train-output/conditioning/models/backup_unet_gn_isotropic_nn9/training/checkpoint_best.pth.tar" --training_patches_fn "training/data/finetuning/test${TEST_REGION}_train_patches_rand_${num_patches}_${random_seed}.txt" --log_fn "${MODELS_DIR}/rand_${num_patches}_${random_seed}/train_results.csv" --model_output_directory "${MODELS_DIR}/rand_${num_patches}_${random_seed}"
+	python training/pytorch/model_finetuning.py --model_file "/mnt/blobfuse/train-output/conditioning/models/backup_unet_gn_isotropic_nn9/training/checkpoint_best.pth.tar" --training_patches_fn "training/data/finetuning/test${TEST_REGION}_train_patches_rand_${num_patches}_${random_seed}.txt" --log_fn "${MODELS_DIR}/rand_${num_patches}_${random_seed}/train_results.csv" --model_output_directory "${MODELS_DIR}/rand_${num_patches}_${random_seed}"
     
 	# Test fine-tuned models
 	MODELS=(
@@ -50,12 +50,11 @@ num_patches=40
 	    "/mnt/blobfuse/train-output/conditioning/models/backup_unet_gn_isotropic_nn9/finetuning/test/test${TEST_REGION}/${num_patches}_patches/rand_${num_patches}_${random_seed}/finetuned_unet_gn.pth_last_k_layers_lr_0.004500_epoch_0_last_k_3.tar"
 	)
 
-	# for model_file in $(ls $MODELS_DIR/rand_${num_patches}*/*_epoch_9*)
-#	for model_file in ${MODELS[*]}
-#	do
-#	    python training/pytorch/test_finetuning.py --area test${TEST_REGION} --model_file "$model_file" --test_tile_fn training/data/finetuning/test${TEST_REGION}_test_tiles.txt --tile_type test
-#	    python training/pytorch/test_finetuning.py --area test${TEST_REGION} --model_file "$model_file" --test_tile_fn training/data/finetuning/test${TEST_REGION}_train_tiles.txt --tile_type train
-#	done
-#    done
-# done    
+	for model_file in ${MODELS[*]}
+	do
+	    python training/pytorch/test_finetuning.py --area test${TEST_REGION} --model_file "$model_file" --test_tile_fn training/data/finetuning/test${TEST_REGION}_test_tiles.txt --tile_type test
+	    python training/pytorch/test_finetuning.py --area test${TEST_REGION} --model_file "$model_file" --test_tile_fn training/data/finetuning/test${TEST_REGION}_train_tiles.txt --tile_type train
+	done
+    done
+done
 
