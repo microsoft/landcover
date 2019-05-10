2c88e1a3416a8a1c27f42635676541da7a67da64
Blake Elias <blelias@microsoft.com>
2019-05-10 00:14:10 +0000

    Add different patch samples for fine tuning training

---
+++ training/pytorch/utils/data/random_patch_samples.sh
@@ -0,0 +1,15 @@
+TEST_REGIONS=(1 2 3 4)
+NUMS_PATCHES=(4 6 10 40 100)
+RANDOM_SEEDS=(1 2 3 4 5)
+
+for region in ${TEST_REGIONS[*]}
+do
+    for num_patches in ${NUMS_PATCHES[*]}
+    do
+	for random_seed in ${RANDOM_SEEDS[*]}
+	do
+	    shuf -n $num_patches training/data/finetuning/test${region}_train_patches.txt > training/data/finetuning/sampled/test${region}_train_patches_rand_${num_patches}_${random_seed}.txt
+	done
+    done
+done
+
