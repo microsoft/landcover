c90a0c2814a741e8dfcbd400d0017c20a29d9598
anthonymlortiz <anthonymlortiz@gmail.com>
2019-05-10 17:29:35 -0700

    setting endpoints back

--- web_tool/endpoints.js
+++ web_tool/endpoints.js
@@ -1,7 +1,11 @@
 ENDPOINTS = [
-    {"url": "http://anthonygn.eastus.cloudapp.azure.com:4444/"}
+    {"url": "http://msrcalebubuntu.eastus.cloudapp.azure.com:4444/"}
 ];
 
 BACKEND_ENDPOINTS = [
-    {"url": "http://anthonygn.eastus.cloudapp.azure.com:4444/"}
+    {"url": "http://msrcalebubuntu.eastus.cloudapp.azure.com:4444/"},
+    {"url": "http://msrcalebubuntu2.eastus.cloudapp.azure.com:4444/"},
+    {"url": "http://msrcalebubuntu2.eastus.cloudapp.azure.com:4445/"},
+    {"url": "http://msrcalebubuntu2.eastus.cloudapp.azure.com:4446/"},
+    {"url": "http://msrcalebubuntu2.eastus.cloudapp.azure.com:4447/"}
 ]
--- web_tool/endpoints_new_labels.js
+++ web_tool/endpoints_new_labels.js
@@ -1,7 +1,11 @@
 ENDPOINTS = [
-    {"url": "http://anthonygn.eastus.cloudapp.azure.com:4445/"}
+    {"url": "http://msrcalebubuntu.eastus.cloudapp.azure.com:4445/"}
 ];
 
 BACKEND_ENDPOINTS = [
-    {"url": "http://anthonygn.eastus.cloudapp.azure.com:4444/"}
+    {"url": "http://msrcalebubuntu.eastus.cloudapp.azure.com:4445/"},
+    {"url": "http://msrcalebubuntu.eastus.cloudapp.azure.com:4445/"},
+    {"url": "http://msrcalebubuntu.eastus.cloudapp.azure.com:4445/"},
+    {"url": "http://msrcalebubuntu.eastus.cloudapp.azure.com:4445/"},
+    {"url": "http://msrcalebubuntu.eastus.cloudapp.azure.com:4445/"},
 ]
