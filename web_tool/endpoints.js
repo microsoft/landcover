070a2a571def4eaaa2a62c6eb95268b2738400cd
Caleb Robinson <calebrob6@gmail.com>
2019-05-13 17:10:36 -0700

    0. Adding default endpoint

--- web_tool/endpoints.js
+++ web_tool/endpoints.js
@@ -3,6 +3,7 @@ ENDPOINTS = [
 ];
 
 BACKEND_ENDPOINTS = [
+    {"url": "http://msrcalebubuntu.eastus.cloudapp.azure.com:4444/"},
     {"url": "http://userstudy1.eastus.cloudapp.azure.com:4444/"},
     {"url": "http://userstudy1.eastus.cloudapp.azure.com:4445/"},
     {"url": "http://userstudy1.eastus.cloudapp.azure.com:4446/"},
