9182cfa9dd08fbd35086924f0a6987e5c531294e
Caleb Robinson <calebrob6@gmail.com>
2019-05-20 14:02:29 -0700

    0. Working

--- web_tool/endpoints.js
+++ web_tool/endpoints.js
@@ -1,9 +1,9 @@
 ENDPOINTS = [
-    {"url": "http://msrcalebubuntu.eastus.cloudapp.azure.com:4444/"}
+    {"url": "http://userstudy1.eastus.cloudapp.azure.com:4444/"}
 ];
 
 BACKEND_ENDPOINTS = [
-    {"url": "http://msrcalebubuntu.eastus.cloudapp.azure.com:4444/"},
+    {"url": "http://userstudy1.eastus.cloudapp.azure.com:4444/"},
     {"url": "http://userstudy1.eastus.cloudapp.azure.com:4444/"},
     {"url": "http://userstudy1.eastus.cloudapp.azure.com:4445/"},
     {"url": "http://userstudy1.eastus.cloudapp.azure.com:4446/"},
