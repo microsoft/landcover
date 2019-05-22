dbb5358b70de7014ebe032baea3db0284a3589e9
Blake Elias <blakeelias@gmail.com>
2019-05-22 00:01:47 +0000

    More results cleaning

--- analysis/compile_offline_exps.sh
+++ analysis/compile_offline_exps.sh
@@ -1,9 +1,9 @@
-TEST_AREAS=(1, 2, 3, 4)
+TEST_AREAS=(1 2 3 4)
 
-results_dir="/mnt/blobfuse/train-output/conditioning/models/backup_unet_gn_isotropic_nn9/finetuning/test/test"
-analysis_dir="/mnt/blobfuse/analysis/active-learning"
+results_dir="/mnt/blobfuse/train-output/conditioning/models/backup_unet_gn_isotropic_nn9/finetuning/test"
+analysis_dir="/mnt/blobfuse/train-output/offline-active-learning"
 
-mkdir ${analysis_dir}
+mkdir -p ${analysis_dir}
 
 
 # Entropy
@@ -11,7 +11,9 @@ mkdir ${analysis_dir}
 # /mnt/blobfuse/train-output/conditioning/models/backup_unet_gn_isotropic_nn9/finetuning/test/test1/entropy/fine_tune_test_results_group_params.csv
 for area in ${TEST_AREAS[@]}
 do
-    file=${analysis_dir}/test${area}/entropy/fine_tune_test_results.csv
+    dir=${analysis_dir}/test${area}/entropy
+    mkdir -p ${dir}
+    file=${dir}/fine_tune_test_results.csv
     cat ${results_dir}/test${area}/entropy/fine_tune_test_results.csv > ${file}
     cat ${results_dir}/test${area}/entropy/fine_tune_test_results_group_params.csv >> ${file}
 done
@@ -21,8 +23,10 @@ done
 # /mnt/blobfuse/train-output/conditioning/models/backup_unet_gn_isotropic_nn9/finetuning/test/test1/random_step_-1/fine_tune_test_results.csv
 for area in ${TEST_AREAS[@]}
 do
-    file=${analysis_dir}/test${area}/random/fine_tune_test_results.csv
-    cp ${results_dir}/test${area}/random_step_-1/fine_tune_test_results.csv > ${file}
+    dir=${analysis_dir}/test${area}/random
+    mkdir -p ${dir}
+    file=${dir}/fine_tune_test_results.csv
+    cat ${results_dir}/test${area}/random_step_-1/fine_tune_test_results.csv > ${file}
 done
 
 
@@ -30,7 +34,9 @@ done
 # /mnt/blobfuse/train-output/conditioning/models/backup_unet_gn_isotropic_nn9/finetuning/test/test4/margin_step_-1/fine_tune_test_results.csv
 for area in ${TEST_AREAS[@]}
 do
-    file=${analysis_dir}/test${area}/margin/fine_tune_test_results.csv
-    cp ${results_dir}/test${area}/margin_step_-1/fine_tune_test_results.csv > ${file}
+    dir=${analysis_dir}/test${area}/margin
+    mkdir -p ${dir}
+    file=${dir}/fine_tune_test_results.csv
+    cat ${results_dir}/test${area}/margin_step_-1/fine_tune_test_results.csv > ${file}
 done
 
---
+++ analysis/sanitize_offline_exps.sh
@@ -0,0 +1,26 @@
+analysis_dir="/mnt/blobfuse/train-output/offline-active-learning"
+
+STRATEGIES=("random" "entropy" "margin")
+TEST_AREAS=(1 2 3 4)
+
+header_grep_query="method, num_points"
+
+for area in ${TEST_AREAS[@]}
+do
+    for strategy in ${STRATEGIES[@]}
+    do
+	# Get the original file here:
+	file=${analysis_dir}/test${area}/${strategy}/fine_tune_test_results.csv
+	
+	# Put the sanitized version here (remove duplicate headers)
+	sanitized=${file}.sanitized
+	
+	# Put just one header line into the sanitized file
+	cat ${file} | grep "${header_grep_query}" | head -n 1 > ${sanitized}
+	# Put actual contents, with no header lines, into sanitized file
+	cat ${file} | grep -v "${header_grep_query}" | grep -v "^>" | grep -v "^->" >> ${sanitized}
+	              # ^^ remove column headers         ^^ remove PDB printouts > and -> 
+	# Overwrite original file with sanitized one
+	mv ${sanitized} ${file}
+    done
+done
