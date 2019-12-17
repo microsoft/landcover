a7aa8f99c1150932edef89082ad0abc529e5d36a
Annie Enchakattu <31044836+Annie-Enchakattu@users.noreply.github.com>
2019-12-17 13:56:03 -0800

    test logging after docker run

--- ARM/run_docker_image.sh
+++ ARM/run_docker_image.sh
@@ -5,3 +5,4 @@ cd landcover
 cd ARM/simple_docker_test
 sudo docker build . -t simple-docker-test:1
 sudo docker run --name test --mount type=bind,source="$(pwd)"/logs,target=/app/logs "simple-docker-test:1"
+sudo echo "hello" >> log.log
