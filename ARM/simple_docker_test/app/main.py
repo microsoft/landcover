8d6425474e4e3f03c93de603ea4f815c9c0f2212
Annie Enchakattu <31044836+Annie-Enchakattu@users.noreply.github.com>
2019-12-17 11:59:38 -0800

    updated logging

--- ARM/simple_docker_test/app/main.py
+++ ARM/simple_docker_test/app/main.py
@@ -2,4 +2,4 @@ import logging
 log = "/app/logs/docker.log"
 
 logging.basicConfig(filename=log,level=logging.DEBUG,format='%(asctime)s %(message)s', datefmt='%d/%m/%Y %H:%M:%S')
-logging.info('this is a test')
+logging.info('testing arm docker image deployment')
