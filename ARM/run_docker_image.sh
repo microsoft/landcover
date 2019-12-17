2b3fb3ac4bb471c01c6175c9c323fec25be6e9d4
Annie Enchakattu <31044836+Annie-Enchakattu@users.noreply.github.com>
2019-12-17 13:40:33 -0800

    updated docker statements

--- ARM/run_docker_image.sh
+++ ARM/run_docker_image.sh
@@ -4,6 +4,6 @@ git checkout feature/vm-arm-template
 cd landcover
 cd ARM/simple_docker_test
 sudo docker build . -t simple-docker-test:1
-sudo docker run -it -d --name test --mount type=bind,source="$(pwd)"/logs,target=/app/logs "simple-docker-test:1"
+sudo docker run --name test --mount type=bind,source="$(pwd)"/logs,target=/app/logs "simple-docker-test:1"
 sudo echo "hello" >> log.log
 sudo echo "$(pwd)" >> log.log
