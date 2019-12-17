88dcaf509ce0caf5e8bc48a3ce588e41cdeb58f5
Annie Enchakattu <31044836+Annie-Enchakattu@users.noreply.github.com>
2019-12-17 09:23:12 -0800

    minor changes for testing

--- ARM/run_docker_image.sh
+++ ARM/run_docker_image.sh
@@ -1,7 +1,8 @@
-git clone https://github.com/microsoft/landcover.git
-cd landcover
-git checkout feature/vm-arm-template
-cd landcover
-cd ARM/simple_docker_test
-sudo docker build . -t simple-docker-test:1
-sudo docker run -it --mount type=bind,source="$(pwd)"/logs,target=/app/logs "simple-docker-test:1"
+#git clone https://github.com/microsoft/landcover.git
+#cd landcover
+#git checkout feature/vm-arm-template
+#cd landcover
+#cd ARM/simple_docker_test
+#sudo docker build . -t simple-docker-test:1
+#sudo docker run -it --mount type=bind,source="$(pwd)"/logs,target=/app/logs "simple-docker-test:1"
+sudo echo "hello" >> log.log
