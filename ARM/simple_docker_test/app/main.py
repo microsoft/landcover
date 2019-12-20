cbdb15f3930a62cfa90e9804a67a806a15c974e0
Annie Enchakattu <31044836+Annie-Enchakattu@users.noreply.github.com>
2019-12-20 14:06:17 -0800

    add subprocess call to run bash command

--- ARM/simple_docker_test/app/main.py
+++ ARM/simple_docker_test/app/main.py
@@ -1,5 +1,9 @@
 import logging
+import subprocess
+
 log = "/app/logs/docker.log"
 
 logging.basicConfig(filename=log,level=logging.DEBUG,format='%(asctime)s %(message)s', datefmt='%d/%m/%Y %H:%M:%S')
 logging.info('testing arm docker image deployment')
+
+subprocess.call(["sudo", "apt", "update"])
