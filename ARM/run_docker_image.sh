fe10fd60f2ba1f209912e1a2547c76bcfbb19e88
Annie Enchakattu <31044836+Annie-Enchakattu@users.noreply.github.com>
2019-12-19 21:44:38 -0800

    Update run_docker_image.sh

--- ARM/run_docker_image.sh
+++ ARM/run_docker_image.sh
@@ -5,5 +5,3 @@ cd landcover
 cd ARM/simple_docker_test
 sudo docker build . -t simple-docker-test:1
 sudo docker run --name test --mount type=bind,source="$(pwd)"/logs,target=/app/logs "simple-docker-test:1"
-sudo echo "hello" >> log.log
-sudo echo "passed in variable $1" >> log.log 
