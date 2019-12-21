b28d72e8591228c0bf9d39f54af41004ace653ff
Caleb Robinson <calebrob6@gmail.com>
2019-12-21 22:19:43 +0000

    Working on self-similarity idea

--- web_tool/Utils.py
+++ web_tool/Utils.py
@@ -65,6 +65,33 @@ COLOR_MAP_LC4 = np.array([
     [0.5,0.375,0.375],
 ], dtype=np.float32)
 
+
+def to_categorical(y, num_classes=None):
+    """Converts a class vector (integers) to binary class matrix.
+    E.g. for use with categorical_crossentropy.
+    # Arguments
+        y: class vector to be converted into a matrix
+            (integers from 0 to num_classes).
+        num_classes: total number of classes.
+    # Returns
+        A binary matrix representation of the input. The classes axis
+        is placed last.
+    """
+    y = np.array(y, dtype='int')
+    input_shape = y.shape
+    if input_shape and input_shape[-1] == 1 and len(input_shape) > 1:
+        input_shape = tuple(input_shape[:-1])
+    y = y.ravel()
+    if not num_classes:
+        num_classes = np.max(y) + 1
+    n = y.shape[0]
+    categorical = np.zeros((n, num_classes), dtype=np.float32)
+    categorical[np.arange(n), y] = 1
+    output_shape = input_shape + (num_classes,)
+    categorical = np.reshape(categorical, output_shape)
+    return categorical
+
+
 def to_one_hot(im, class_num):
     one_hot = np.zeros((class_num, im.shape[-2], im.shape[-1]), dtype=np.float32)
     for class_id in range(class_num):
