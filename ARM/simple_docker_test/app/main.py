dec84e0cfb8752201d447ec7a56de9d228991c53
Annie Enchakattu <31044836+Annie-Enchakattu@users.noreply.github.com>
2019-12-21 11:48:50 -0800

    call script to deallocate vm

--- ARM/simple_docker_test/app/main.py
+++ ARM/simple_docker_test/app/main.py
@@ -1,7 +1,10 @@
 import logging
 import subprocess
+import shlex
 
 log = "/app/logs/docker.log"
 
 logging.basicConfig(filename=log,level=logging.DEBUG,format='%(asctime)s %(message)s', datefmt='%d/%m/%Y %H:%M:%S')
 logging.info('testing arm docker image deployment')
+
+subprocess.call(shlex.split('bash deallocate_vm.sh'))
