8e0bb36aa786a0860018d59dbaf780f4a40319a2
Annie Enchakattu <31044836+Annie-Enchakattu@users.noreply.github.com>
2019-12-22 01:30:41 -0800

    changed az config to python file

--- ARM/build_run_docker_image.sh
+++ ARM/build_run_docker_image.sh
@@ -6,9 +6,9 @@ git checkout feature/vm-arm-template
 cd landcover
 cd ARM/simple_docker_test
 
-cat << EOF > app/az.config
-export AZ_RESOURCE_GRP=$1
-export AZ_RESOURCE_NAME=$2
+cat << EOF > app/az_config.py
+AZ_RESOURCE_GRP="$1"
+AZ_RESOURCE_NAME="$2"
 EOF
 
 sudo docker build . -t simple-docker-test:1
