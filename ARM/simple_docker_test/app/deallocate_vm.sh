88e444fb95bb6e3460e3bc5f739f75854177d822
Annie Enchakattu <31044836+Annie-Enchakattu@users.noreply.github.com>
2019-12-21 17:36:03 -0800

    replaced parameters with environment variables

--- ARM/simple_docker_test/app/deallocate_vm.sh
+++ ARM/simple_docker_test/app/deallocate_vm.sh
@@ -1,5 +1,5 @@
-resource_grp= $1
-vm_name = $2
+resource_grp= $az_resource_grp
+vm_name = $az_vm_name
 sudo echo "deallocating vm" >> log.log
 az login --identity
 az vm deallocate -g $resource_grp -n $vm_name
